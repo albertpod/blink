2056ffe788596cc27d4fceafbbc5c0eb1a42a9e1
AlbertPodusenko <albert.podusenko@gmail.com>
2016-04-17 22:38:03 +0300

    recovered2

---
+++ Blink/ViewController.swift
@@ -0,0 +1,156 @@
+import UIKit
+import AVFoundation
+
+var cameraView : UIView!
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    var counter = 5
+    var uiImage : UIImage?
+    var timer = NSTimer()
+    var detected = false
+    var detectedTime : NSDate?
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var videoCaptureOutput = AVCaptureVideoDataOutput()
+    // If we find a device we'll store it here for later use
+    var captureDevice : AVCaptureDevice?
+    
+    func timerAction() {
+            counter -= 1
+            timerLabel.text = "\(counter)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        /*let darkBlur = UIBlurEffect(style: UIBlurEffectStyle.Dark)
+        let blurView = UIVisualEffectView(effect: darkBlur)
+        blurView.frame = self.view.bounds
+        self.view.addSubview(blurView)*/
+        print(self.view.bounds)
+        
+        cameraView = UIView(frame: CGRectMake(0, self.view.frame.width / 4, self.view.frame.size.width, self.view.frame.size.width))
+        cameraView!.backgroundColor = UIColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 1.0)
+        cameraView!.alpha = 0
+        //cameraView!.center = self.view.center;
+        self.view.addSubview(cameraView!)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        captureSession.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let devices = AVCaptureDevice.devices()
+        
+        // Loop through all the capture devices on this phone
+        for device in devices {
+            // Make sure this particular device supports video
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                // Finally check the position and confirm we've got the back camera
+                if(device.position == AVCaptureDevicePosition.Front) {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        print("Capture device found")
+                        beginSession()
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    override func  preferredStatusBarStyle()-> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            do {
+                try device.lockForConfiguration()
+                
+            } catch let error as NSError {
+                print(error.code)
+            }
+            device.unlockForConfiguration()
+        }
+        
+    }
+    
+    func beginSession() {
+        
+        var bounds : CGRect
+
+        let cameraQueue = dispatch_queue_create("cameraQueue", DISPATCH_QUEUE_SERIAL)
+        
+        configureDevice()
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        } catch let error as NSError {
+            print(error.code)
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        bounds = cameraView!.layer.frame
+        previewLayer!.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewLayer!.bounds = bounds
+        previewLayer!.position = CGPointMake(CGRectGetMidX(bounds), CGRectGetMidY(bounds))
+        //previewLayer!.cornerRadius = 10
+        
+        //previewLayer!.borderColor = UIColor.whiteColor().colorWithAlphaComponent(0.5).CGColor
+        self.view.layer.addSublayer(previewLayer!)
+        
+        videoCaptureOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey:Int(kCVPixelFormatType_32BGRA)]
+        
+        videoCaptureOutput.setSampleBufferDelegate(self, queue: cameraQueue)
+        
+        captureSession.addOutput(videoCaptureOutput)
+        
+        captureSession.startRunning()
+        
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput!, didDropSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection:AVCaptureConnection!) {
+        
+        //print("frame dropped")
+    }
+    
+    func captureOutput(captureOutput: AVCaptureOutput, didOutputSampleBuffer sampleBuffer: CMSampleBufferRef, fromConnection connection: AVCaptureConnection) {
+        print("frame recieved")
+        let pixelBuffer: CVImageBufferRef = CMSampleBufferGetImageBuffer(sampleBuffer)!
+        CVPixelBufferLockBaseAddress(pixelBuffer, 0)
+        uiImage = imageFromSampleBuffer(pixelBuffer)
+        uiImage = uiImage?.imageRotatedByDegrees(90, flip: false)
+        uiImage = uiImage?.cropsToSquare()
+        if counter < 1 {
+            captureSession.stopRunning()
+            detected = false
+            performSegueWithIdentifier("showPhoto", sender : nil)
+            timer.invalidate()
+            counter = 5
+        }
+        let faceHaarPath = NSBundle.mainBundle().pathForResource("face", ofType:"xml")
+        let eyesHaarPath = NSBundle.mainBundle().pathForResource("eyes", ofType:"xml")
+        let openedEyePath = NSBundle.mainBundle().pathForResource("opened_eye", ofType:"xml")
+        
+        (detected, _) = detectEyeBlink(uiImage)
+        if  detected/*OpenCVWrapper.processImageWithOpenCV(uiImage, faceHaarPath, eyesHaarPath, openedEyePath)*/ {
+            /*dispatch_async(dispatch_get_main_queue(), {
+                self.shareToInstagram(uiImage!)
+            })*/
+            print("hho")
+            timer = NSTimer(fireDate: NSDate().dateByAddingTimeInterval(0), interval: 1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+            NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+        }
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0)
+        
+        //UIImageWriteToSavedPhotosAlbum(uiImage!, self, "imageSaveMethod:didFinishSavingWithError:contextInfo:", nil);
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "showPhoto"){
+            if let destination = segue.destinationViewController as? PhotoViewController {
+                destination.postingPhoto = uiImage
+            }
+        }
+    }
+}
---
+++ UIImageExtension.swift
@@ -0,0 +1,92 @@
+//
+//  UIImageExtension.swift
+//  Blink
+//
+//  Created by Albert Podusenko on 22.03.16.
+//  Copyright © 2016 Albert Podusenko. All rights reserved.
+//
+
+import Foundation
+
+extension UIImage {
+    public func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
+        
+        let degreesToRadians: (CGFloat) -> CGFloat = {
+            return $0 / 180.0 * CGFloat(M_PI)
+        }
+        
+        // calculate the size of the rotated view's containing box for our drawing space
+        let rotatedViewBox = UIView(frame: CGRect(origin: CGPointZero, size: size))
+        let t = CGAffineTransformMakeRotation(degreesToRadians(degrees));
+        rotatedViewBox.transform = t
+        let rotatedSize = rotatedViewBox.frame.size
+        
+        // Create the bitmap context
+        UIGraphicsBeginImageContext(rotatedSize)
+        let bitmap = UIGraphicsGetCurrentContext()
+        
+        // Move the origin to the middle of the image so we will rotate and scale around the center.
+        CGContextTranslateCTM(bitmap, rotatedSize.width / 2.0, rotatedSize.height / 2.0);
+        
+        //   // Rotate the image context
+        CGContextRotateCTM(bitmap, degreesToRadians(degrees));
+        
+        // Now, draw the rotated/scaled image into the context
+        var yFlip: CGFloat
+        
+        if(flip){
+            yFlip = CGFloat(-1.0)
+        } else {
+            yFlip = CGFloat(1.0)
+        }
+        
+        CGContextScaleCTM(bitmap, yFlip, -1.0)
+        CGContextDrawImage(bitmap, CGRectMake(-size.width / 2, -size.height / 2, size.width, size.height), CGImage)
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
+    public func cropsToSquare() -> UIImage {
+        let refWidth = CGFloat(CGImageGetWidth(self.CGImage))
+        let refHeight = CGFloat(CGImageGetHeight(self.CGImage))
+        let cropSize = refWidth > refHeight ? refHeight : refWidth
+        
+        let x = (refWidth - cropSize) / 2.0
+        let y = (refHeight - cropSize) / 2.0
+        
+        let cropRect = CGRectMake(x, y, cropSize, cropSize)
+        let imageRef = CGImageCreateWithImageInRect(self.CGImage, cropRect)
+        let cropped = UIImage(CGImage: imageRef!, scale: 0.0, orientation: self.imageOrientation)
+        
+        return cropped
+    }
+
+}
+
+/*
+http://stackoverflow.com/questions/14383932/convert-cmsamplebufferref-to-uiimage
+*/
+func imageFromSampleBuffer(pixelBuffer: CVImageBuffer) -> UIImage? {
+    let colorSpace = CGColorSpaceCreateDeviceRGB()
+    CVPixelBufferLockBaseAddress(pixelBuffer, 0)
+    let address = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0)
+    let bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer)
+    let width = CVPixelBufferGetWidth(pixelBuffer)
+    let height = CVPixelBufferGetHeight(pixelBuffer)
+    
+    let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.PremultipliedFirst.rawValue | CGBitmapInfo.ByteOrder32Little.rawValue)
+    
+    let context = CGBitmapContextCreate(address, width, height, 8,bytesPerRow, colorSpace, bitmapInfo.rawValue);
+    let imageRef = CGBitmapContextCreateImage(context)
+    
+    CVPixelBufferUnlockBaseAddress(pixelBuffer, 0)
+    var resultImage : UIImage?
+    if context != nil {
+        resultImage = UIImage(CGImage: imageRef!)
+    }
+    
+    return resultImage
+}
